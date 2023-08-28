72ed2e713e6aa48cbf4006563b51bf630602b626
Santiago Caritat <6317@holbertonstudents.com>
2023-08-28 09:27:50 -0700

    task 5

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3
 """task 5"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """sum of list"""
+    num_float = 0.0
+    for num in input_list:
+        num_float += num
+    return num_float
