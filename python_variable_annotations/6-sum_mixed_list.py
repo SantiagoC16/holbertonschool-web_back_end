ff811e33dd864f5113f7d2601b909fe7d0d3364b
Santiago Caritat <6317@holbertonstudents.com>
2023-08-28 09:37:57 -0700

    task 6

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3
 """task 6"""
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[float]) -> float:
+    """sum int and float and return float"""
+    num_float = 0.0
+    for num in mxd_lst:
+        num_float += num
+    return num_float
