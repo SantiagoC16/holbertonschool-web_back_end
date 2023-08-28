6809df3c0be014220080f8926684b481e7186e2f
Santiago Caritat <6317@holbertonstudents.com>
2023-08-28 09:48:27 -0700

    task 7

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python3
 """task 7"""
+from typing import List, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> tuple:
+    """return a tuple with an str and int/float"""
+    return tuple(k, v)
