1866da3f91c70adb77f43e62fb410f63dcab50b3
Santiago Caritat <6317@holbertonstudents.com>
2023-08-28 09:59:25 -0700

    task 8

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3
 """task 8"""
+from typing import Tuple, Union, Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """float and return a multiply of float"""
+    def multiply(num_float: float) -> float:
+        return num_float * multiplier
+    return multiply
