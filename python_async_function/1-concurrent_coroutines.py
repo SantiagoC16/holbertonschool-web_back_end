5eabed41db2d6e192d6140cb00f195b1a60aa32a
Santiago Caritat <6317@holbertonstudents.com>
2023-08-30 06:53:22 -0700

    task 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python3
 """task 1"""
+
+import random
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """return a list of all delays"""
+    list_delays = []
+    for n_delays in range(n):
+        list_delays.append(await wait_random(max_delay))
+    return sorted(list_delays)
