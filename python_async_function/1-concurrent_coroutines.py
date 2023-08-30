f336df51f18d4e7bbc1a35e2a344f3f5f9f2412e
Santiago Caritat <6317@holbertonstudents.com>
2023-08-30 06:57:36 -0700

    task 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -10,6 +10,6 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """return a list of all delays"""
     list_delays = []
-    for n_delays in range(n):
+    for i in range(n):
         list_delays.append(await wait_random(max_delay))
     return sorted(list_delays)
