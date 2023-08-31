9871c8417e3b8263bfd37b3613e48831eb289a6a
Santiago Caritat <6317@holbertonstudents.com>
2023-08-31 09:59:40 -0700

    task 1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3
 """task 1"""
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """a function that collect 10 random numbers and return them"""
+    nums_to_return = [x async for x in async_generator()]
+    return nums_to_return
