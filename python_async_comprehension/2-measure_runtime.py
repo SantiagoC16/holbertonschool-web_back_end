53da1e09f3722393ec712645fa04935002ac8729
Santiago Caritat <6317@holbertonstudents.com>
2023-08-31 10:07:04 -0700

    task 2

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python3
 """task 2"""
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """measure the total runtime and return it"""
+    start = time.time()
+    await asyncio.gather(async_comprehension(),
+                         async_comprehension(),
+                         async_comprehension(),
+                         async_comprehension())
+    end = time.time()
+    total_time = end - start
+    return total_time
