434527ee81695a8dd70c990f499e68b544bc37f2
Santiago Caritat <6317@holbertonstudents.com>
2023-08-31 09:53:04 -0700

    task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,6 +7,6 @@ from typing import Generator
 
 async def async_generator() -> Generator[float, None, None]:
     """return a list with 10 generated numbers"""
-    for i in range(11):
+    for i in range(10):
         await asyncio.sleep(1)
         yield uniform(0, 10)
