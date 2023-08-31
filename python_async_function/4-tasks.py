393625f465c224ad206177875281bb2db4a58a61
Santiago Caritat <6317@holbertonstudents.com>
2023-08-31 06:49:32 -0700

    task 4

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -6,7 +6,7 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """return a list of all delays"""
     list_delays = []
     for i in range(n):
