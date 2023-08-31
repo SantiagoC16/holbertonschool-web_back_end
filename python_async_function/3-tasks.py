dcb553cb348e7ddbe2cf09379e6a1e66b2928f09
Santiago Caritat <6317@holbertonstudents.com>
2023-08-31 06:39:48 -0700

    task 3

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -2,13 +2,12 @@
 """task 3"""
 import asyncio
 import time
+import random
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def measure_time(n: int, max_delay: int) -> float:
-    """a function that measures the total execution time"""
-    start = time.time()
-    asyncio.run(wait_n(n, max_delay))
-    end = time.time()
-    total_time = end - start
-    return total_time / n
+def task_wait_random(max_delay: int) -> float:
+    """returns a asyncio.Task"""
+
+    Task = asyncio.create_task(wait_random(max_delay))
+    return Task
