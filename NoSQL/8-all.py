e35a50531a10c98788e81b23c71830ee0cca8738
Santiago Caritat <6317@holbertonstudents.com>
2023-09-05 09:54:05 -0700

    task 8

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -4,6 +4,7 @@ import pymongo
 
 
 def list_all(mongo_collection):
+    """a function that lists all documents in a collection"""
     list_documents = mongo_collection.find()
     if list_documents is None:
         return []
