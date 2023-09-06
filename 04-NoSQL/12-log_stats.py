b1215f131b38ec5bac58b5633071f785c5823a49
SantiagoC16 <6317@holbertonstudents.com>
2023-09-06 15:04:55 -0700

    rename folder

--- 04-NoSQL/12-log_stats.py
+++ 04-NoSQL/12-log_stats.py
@@ -7,18 +7,19 @@ insert_school = __import__('9-insert_school').insert_school
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
+
     print("{} logs".format(nginx_collection.count_documents({})))
     print("Methods:")
     get = nginx_collection.count_documents({"method": "GET"})
     print("\tmethod GET: {}".format(get))
-    get = nginx_collection.count_documents({"method": "POST"})
-    print("\tmethod POST: {}".format(get))
-    get = nginx_collection.count_documents({"method": "PUT"})
-    print("\tmethod PUT: {}".format(get))
-    get = nginx_collection.count_documents({"method": "PATCH"})
-    print("\tmethod PATCH: {}".format(get))
-    get = nginx_collection.count_documents({"method": "DELETE"})
-    print("\tmethod DELETE: {}".format(get))
-    get = nginx_collection.count_documents(
+    post = nginx_collection.count_documents({"method": "POST"})
+    print("\tmethod POST: {}".format(post))
+    put = nginx_collection.count_documents({"method": "PUT"})
+    print("\tmethod PUT: {}".format(put))
+    patch = nginx_collection.count_documents({"method": "PATCH"})
+    print("\tmethod PATCH: {}".format(patch))
+    delete = nginx_collection.count_documents({"method": "DELETE"})
+    print("\tmethod DELETE: {}".format(delete))
+    get_status = nginx_collection.count_documents(
         {"method": "GET", "path": "/status"})
-    print("{} status check".format(get))
+    print("{} status check".format(get_status))
