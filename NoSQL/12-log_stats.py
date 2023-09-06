b2a3ce5100084f7cba51f766bf2e7af99a0e1e7e
SantiagoC16 <6317@holbertonstudents.com>
2023-09-06 14:36:48 -0700

    task 12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -3,7 +3,22 @@
 from pymongo import MongoClient
 list_all = __import__('8-all').list_all
 insert_school = __import__('9-insert_school').insert_school
-schools_by_topic = __import__('11-schools_by_topic').schools_by_topic
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+    print("{} logs".format(nginx_collection.count_documents({})))
+    print("Methods:")
+    get = nginx_collection.count_documents({"method": "GET"})
+    print("\tmethod GET: {}".format(get))
+    get = nginx_collection.count_documents({"method": "POST"})
+    print("\tmethod POST: {}".format(get))
+    get = nginx_collection.count_documents({"method": "PUT"})
+    print("\tmethod PUT: {}".format(get))
+    get = nginx_collection.count_documents({"method": "PATCH"})
+    print("\tmethod PATCH: {}".format(get))
+    get = nginx_collection.count_documents({"method": "DELETE"})
+    print("\tmethod DELETE: {}".format(get))
+    get = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"})
+    print("{} status check".format(get))
