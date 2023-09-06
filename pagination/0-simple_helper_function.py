6dedd3cd36e9757e26cb2d46316b5c78820a2103
SantiagoC16 <6317@holbertonstudents.com>
2023-09-06 14:52:56 -0700

    task 0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -2,5 +2,8 @@
 """task 0"""
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    
+    """pagination"""
+    _tuple = ((page - 1) * page_size, page * page_size)
+    return _tuple
