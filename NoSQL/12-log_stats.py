1432a82d39e8feef8d4c5a009926a6616689db02
Santiago Caritat <6317@holbertonstudents.com>
2023-09-06 07:02:42 -0700

    task 12

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""task 12"""
+from pymongo import MongoClient
+list_all = __import__('8-all').list_all
+insert_school = __import__('9-insert_school').insert_school
+schools_by_topic = __import__('11-schools_by_topic').schools_by_topic
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
