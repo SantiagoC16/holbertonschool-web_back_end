3b645d8d27b528277cea8ad5beca03e09b0bb32a
SantiagoC16 <6317@holbertonstudents.com>
2023-09-06 14:59:25 -0700

    test

--- NoSQL/10-update_topics.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""task 10"""
-import pymongo
-
-
-def update_topics(mongo_collection, name, topics):
-    """a function that changes all topics of a school document based on the name"""
-    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
--- NoSQL/11-schools_by_topic.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""task 11"""
-import pymongo
-
-
-def schools_by_topic(mongo_collection, topic):
-    """a function that returns the list of school having a specific topic"""
-    return mongo_collection.find({"topics": topic})
