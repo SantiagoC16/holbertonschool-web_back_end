abe58ed603fce73b42f17febaf9592e2fbfcf3a3
SantiagoC16 <6317@holbertonstudents.com>
2023-09-07 10:23:45 -0700

    task 2

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -36,7 +36,7 @@ class Server:
     def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
         """return a dict"""
         self.dataset()
-        total_pages = len(self.__dataset) / page_size
+        total_pages = len(self.__dataset)
         data = self.get_page(page, page_size)
         if page + 1 > total_pages:
             next_page = None
