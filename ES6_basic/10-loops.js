e07f8ddf4da31fbb22632912c5429be2fe0047f9
Santiago Caritat <6317@holbertonstudents.com>
2023-09-14 09:56:50 -0700

    task 10

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const new_array = []
+  const new_array = [];
   for (const idx of array) {
-    const concat = `${appendString}${idx}`
-    new_array.push(concat)
+    const concat = `${appendString}${idx}`;
+    new_array.push(concat);
   }
 
   return new_array;
