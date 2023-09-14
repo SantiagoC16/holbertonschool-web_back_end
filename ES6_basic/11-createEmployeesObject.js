faded2ada5cfd49720abca933fc963cbd1e31130
Santiago Caritat <6317@holbertonstudents.com>
2023-09-14 10:28:04 -0700

    task 11

--- ES6_basic/11-createEmployeesObject.js
+++ ES6_basic/11-createEmployeesObject.js
@@ -1,8 +1,3 @@
 export default function createEmployeesObject(departmentName, employees) {
-  let dict = {};
-  const array = [];
-  for (const idx of employees)
-    array.push(idx);
-  dict = { departmentName, array }
-  return dict
+  return { [`${departmentName}`]: employees, };
 }
