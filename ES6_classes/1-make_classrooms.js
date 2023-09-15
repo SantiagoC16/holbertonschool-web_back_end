58562a2c7324c1f7a535fe9609db35ba49e3148e
Santiago Caritat <6317@holbertonstudents.com>
2023-09-15 07:32:36 -0700

    task 1

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,9 +1,8 @@
-import ClassRoom from "./0-classroom";
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-    const first = new ClassRoom(19);
-    const second = new ClassRoom(20);
-    const third = new ClassRoom(34);
-    const array = [first, second, third];
-    return array;
+  const first = new ClassRoom(19);
+  const second = new ClassRoom(20);
+  const third = new ClassRoom(34);
+  return [first, second, third];
 }
