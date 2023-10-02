b0876ecc42d53c346f46dfee6462dafc06d4a469
Santiago Caritat <6317@holbertonstudents.com>
2023-10-02 10:28:40 -0700

    task 2

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,10 +1,12 @@
 export default function handleResponseFromAPI(promise) {
   return promise
     .then((result) => {
-      result = 'Got a response from the API'
       console.log(result);
     })
     .catch((error) => {
-      console.log(error)
+      console.log(error);
+    })
+    .finally(() => {
+      console.log('Got a response from the API');
     });
 }
